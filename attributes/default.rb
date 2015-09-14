17d98c41a72b9b8b80e168d7e82da6f964c6f8f7
anoworl <anoworl@gmail.com>
2015-09-14 10:35:29 +0900

    modify version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['play2']['version'] = "2.1.1"
+default['play2']['version'] = "1.3.6"
 default['play2']['url'] = "http://downloads.typesafe.com/typesafe-activator/"
 default['play2']['checksum'] = "1ec0b736f9836f760b3c135154fd9a4b"
 default['play2']['home'] = '/usr/local/play'
